ee0c6b8610935044f8046ab80717e02b66acc9de
197651 <197651@LTIN117934.cts.com>
2019-11-25 22:55:29 +0530

    spring cloud properties

--- cloud-config-server/src/main/java/io/jc/cloud/config/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/io/jc/cloud/config/CloudConfigServerApplication.java
@@ -2,7 +2,10 @@ package io.jc.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
 @SpringBootApplication
 public class CloudConfigServerApplication {
 
